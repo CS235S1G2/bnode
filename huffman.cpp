c1793a257dd28c4f7ccf1733f204945e6c045f10
Justin Chandler <justinchandler.17@gmail.com>
2017-06-10 20:01:20 -0600

    Create huffman.cpp

--- huffman.cpp
+++ huffman.cpp
@@ -120,6 +120,7 @@ string * Huffman::find(const string item)
 
 string * Huffman::findRecursive(BinaryNode <Pair <string, float> > * tree, const string item)
 {
+	cerr << "Findfunction";
 	string huffman1(""); // one huffman code
 	string *huffmanCode = NULL; // pointer to the huffman code which will be passed on
 	if (tree->pLeft != NULL)  //if there is a node to the left GO LEFT
